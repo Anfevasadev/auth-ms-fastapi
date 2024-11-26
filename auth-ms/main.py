ede3ee78283dd7444da75441ff569f1e4036d99d
Andrés Felipe Valencia Sánchez <47144203+Anfevasadev@users.noreply.github.com>
2024-11-26 15:55:46 +0000

    Agregar enrutadores para autenticación y usuarios, y manejar errores 404

--- auth-ms/main.py
+++ auth-ms/main.py
@@ -1,8 +1,15 @@
+# auth-ms/main.py
 from fastapi import FastAPI
+from interfaces.api.v1.user import router as user_router
+from interfaces.api.v1.auth import router as auth_router
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
+app.include_router(auth_router, prefix="/api/v1/auth")
+app.include_router(user_router, prefix="/api/v1/users")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+@app.exception_handler(404)
+async def not_found_handler(request, exc):
+    return JSONResponse(status_code=404, content={"message": "Not Found"})
