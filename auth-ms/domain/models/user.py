d02b674a09a27e4b67610259f700d720097e5035
Andrés Felipe Valencia Sánchez <47144203+Anfevasadev@users.noreply.github.com>
2024-11-26 19:15:03 +0000

    Agregar esquemas de autenticación y configuración de base de datos, y actualizar Dockerfile con pip

---
+++ auth-ms/domain/models/user.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, String, Enum
+from sqlalchemy.dialects.postgresql import UUID
+from infrastructure.database.session import Base
+import uuid
+import enum
+
+
+class UserRole(enum.Enum):
+    admin = 'admin'
+    user = 'user'
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    user_id = Column(UUID(as_uuid=True),
+                     primary_key=True,
+                     index=True,
+                     unique=True,
+                     nullable=False,
+                     default=uuid.uuid4)
+    username = Column(String, unique=True)
+    password_hash = Column(String, nullable=False)
+    email = Column(String, unique=True)
+    role = Column(Enum(UserRole), default=UserRole.user)
---
+++ auth-ms/interfaces/api/v1/schemas/auth_schema.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+
+class Login(BaseModel):
+    email: str
+    password: str
+
+
+class Register(BaseModel):
+    username: str
+    email: str
+    password: str
